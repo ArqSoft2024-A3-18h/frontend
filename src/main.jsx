64c2ea7fe9e36e8f608a7d4f894c49cac3249aef
luisda <lusanchezmo@unal.edu.co>
2025-01-25 10:31:48 -0500

    solucion de error de ./apolloClient

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { ApolloProvider } from "@apollo/client";
 import App from "./App";
-import client from "apolloClient";
+import client from "./apolloClient.js";
 
 // Obtén el contenedor principal de tu aplicación
 const container = document.getElementById("root");
