9794d6f900dc7db8f419ca8287c55e8ee41a8171
grevy0810 <grrinconm@unal.edu.co>
2025-02-11 20:08:50 -0500

    feat: add form list

--- src/pages/Forms.tsx
+++ src/pages/Forms.tsx
@@ -1,11 +1,86 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+import ButtonGroup from '@mui/material/ButtonGroup';
 
+const cards = [
+    {
+      id: 1,
+      title: 'Plants',
+      questionsCount: '10',
+    },
+    {
+      id: 2,
+      title: 'Animals',
+      questionsCount: '15',
+    },
+    {
+      id: 3,
+      title: 'Humans',
+      questionsCount: '5',
+    },
+    {
+        id: 4,
+        title: 'Humans',
+        questionsCount: '2',
+    },
+    {
+        id: 5,
+        title: 'Humans',
+        questionsCount: '30',
+    },
+];
 const Forms = () => {
     return(
-        <div>
-            <h1>Forms List</h1>
-            <Link to="new" className="text-blue-500">Create New Form</Link>
+        <div className="m-4">
+            <div className="flex flex-row justify-between items-center mb-4 ">
+                <h1>Formularios</h1>
+                <Button variant="contained" href="/my/forms/new" >Nuevo Form</Button>
+
+            </div>
+            <Box
+            sx={{
+                width: '100%',
+                display: 'grid',
+                gridTemplateColumns: 'repeat(auto-fill, minmax(min(300px, 100%), 1fr))',
+                gap: 2,
+            }}
+            >
+            {cards.map((card, index) => (
+                <Card>
+                    <CardActionArea
+                        sx={{
+                        height: '100%',
+                        '&[data-active]': {
+                            backgroundColor: 'action.selected',
+                            '&:hover': {
+                            backgroundColor: 'action.selectedHover',
+                            },
+                        },
+                        }}
+                    >
+                        <CardContent sx={{ height: '100%' }}>
+                            <Typography variant="h5" component="div">
+                                {card.title}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {card.questionsCount} preguntas
+                            </Typography>
+                            <ButtonGroup variant="text" aria-label="small button group">
+                                <Button>Crear Juego</Button>
+                                <Button color="error">Eliminar</Button>
+                            </ButtonGroup>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            ))}
+            </Box>
+
         </div>
     )
 
